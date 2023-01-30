088cb3663d97120810e2c577189b7d3da5881999
David Erb <david.erb@diamond.ac.uk>
2023-01-30 05:24:08 +0000

    refactors names

---
+++ .dae-devops/prepare_git_dependencies.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ********** Please don't edit this file!
+# ********** It has been generated automatically by dae_devops version 0.3.3.
+# ********** For repository_name dls-logformatter
+
+me=${BASH_SOURCE}
+echo "${me}: installing 0 dependencies for repository_name dls-logformatter"
+
+function __install {
+    echo ${me}: "$@"
+    "$@"
+}
+
+
+# dae_devops_fingerprint 54342904c9822c5d4e1056d5950b458b
